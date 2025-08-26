060c529ab7a68a57e0705ade3cff1852afd1b9dc
Electro-Corp <chinmaytanmay@outlook.com>
2025-08-26 09:16:21 -0600

    remove old build folder

--- build_/CMakeFiles/Nexus.dir/Engine/transform.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Nexus.dir/Engine/transform.cpp.o: \
- /home/segfault/nexus/Engine/transform.cpp /usr/include/stdc-predef.h \
- /home/segfault/nexus/Engine/transform.h
--- build_/shader.sh
+++
@@ -1,2 +0,0 @@
-glslc ../TestProgram/Shaders/shader.vert -o ../shaders/vert.spv
-glslc ../TestProgram/Shaders/shader.frag -o ../shaders/frag.spv
